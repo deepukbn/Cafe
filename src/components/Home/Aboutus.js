cde5a3d73ee81b11f9a15092eb42b91c4ec0f41f
deepukbn <k.bdeepu471999@gmail.com>
2021-06-09 12:13:31 +0530

    Revert "changes updated"
    This reverts commit bb619cf4da1e8d8ddd72a3aa59a7362579e3488a.

--- src/components/Home/Aboutus.js
+++ src/components/Home/Aboutus.js
@@ -32,6 +32,8 @@ const Aboutus = ({ data }) => {
                 Address:Scoopy's Cafe 10th Cross, Shankarmutt Road, K R Puram,
                 Hassan
               </h3>
+            </div>
+            <div className="col-10 col-sm-8 mx-auto text-center">
               <h3>Phone Number:8277322858</h3>
             </div>
           </div>
